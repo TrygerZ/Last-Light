2dde1cc7a0cb1c50c3cabfad37e56aaf4debf023
TrygerZ <kevinutomoz24@gmail.com>
2026-05-10 22:12:27 +0700

    menambahan fitur pickup wood dengan menekan tombol E, mengganti desain campfire dengan asset original mengganti asset MC dengan asset orignal menambahkan animasi pada camp fire

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,22 @@ public class Player : MonoBehaviour
    public float movespeed = 5f;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-   
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * movespeed, rb.linearVelocity.y);
+
+        if (moveInput > 0)
+            sprite.flipX = true;
+        else if (moveInput < 0)
+            sprite.flipX = false;
     }
 }
