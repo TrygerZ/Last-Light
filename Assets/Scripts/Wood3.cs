d6ff7d151b17e720cccc7a95e44f8f6d576e2a7e
kevinutomoz24@gmail.com <>
2026-05-11 00:04:22 +0700

    Fitur Backpack tidak jadi, disimpan untuk nanti
    Sedikit rework pada spawner, sekarang terdapat 2 spawner di kiri dan kanan sehingga menghindari kayu spawn di base
    Menambahkan jenis kayu dan juga setiap value dari kayu

---
+++ Assets/Scripts/Wood3.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Wood3 : MonoBehaviour
+{
+    [SerializeField] private float timeValue = 20f;
+
+    private Wood3Spawner spawner;
+    private bool isPickedUp;
+    private bool playerInRange;
+
+    public void SetSpawner(Wood3Spawner spawnerRef)
+    {
+        spawner = spawnerRef;
+    }
+
+    private void Update()
+    {
+        if (!playerInRange || isPickedUp)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (IsPlayer(other))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (IsPlayer(other))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private bool IsPlayer(Collider2D other)
+    {
+        if (other == null) return false;
+        if (other.CompareTag("Player")) return true;
+        if (other.transform.root.CompareTag("Player")) return true;
+        if (other.GetComponentInParent<Player>() != null) return true;
+        return false;
+    }
+
+    private void PickUp()
+    {
+        isPickedUp = true;
+
+        if (spawner != null)
+        {
+            spawner.OnWoodPickedUp();
+        }
+
+        Destroy(gameObject);
+    }
+}
