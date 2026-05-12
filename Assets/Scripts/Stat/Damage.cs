a5f89b94f776014b514fd69f3f62122d3b368e46
Groomball <praxion255@gmail.com>
2026-05-12 19:45:57 +0700

    Added patrol and dps for enemy patrol in enemymovement script and dps in damage script

--- Assets/Scripts/Stat/Damage.cs
+++ Assets/Scripts/Stat/Damage.cs
@@ -1,6 +1,49 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
     public int damage;
+    [SerializeField] float damageCooldown;
+    [SerializeField] private bool isEnemy;
+    private Health health;
+    private float timeCheck;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damage otherDamage = collision.GetComponent<Damage>();
+        if (otherDamage != null)
+        {
+            if (otherDamage.isEnemy == isEnemy)
+            {
+                return;
+            }
+        }
+
+        health = collision.GetComponent<Health>();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (health != null)
+        {
+            health = null;
+            timeCheck = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (health != null)
+        {
+            timeCheck += Time.deltaTime;
+            if (timeCheck >= damageCooldown)
+            {
+                health.TakeDamage(damage);
+                timeCheck = 0;
+            }
+        }
+    }
 }
