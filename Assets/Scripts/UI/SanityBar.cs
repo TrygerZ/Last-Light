181bb5c5bcdf1205c735b0c0725c96a5b7378e3a
Groomball <praxion255@gmail.com>
2026-05-13 09:19:55 +0700

    Added sanitybar not done yet

---
+++ Assets/Scripts/UI/SanityBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class SanityBar : MonoBehaviour
+{
+    public Slider slider;
+    public Health health;
+
+    private void Start()
+    {
+        health = GameObject.FindGameObjectWithTag("PlayerBody").GetComponent<Health>();
+        slider = GetComponent<Slider>();
+        slider.maxValue = health.MaxHealth;
+        slider.value = health.MaxHealth;
+    }
+    public void setHealth(int currenthealth)
+    {
+        slider.value = currenthealth;
+    }
+
+
+}
